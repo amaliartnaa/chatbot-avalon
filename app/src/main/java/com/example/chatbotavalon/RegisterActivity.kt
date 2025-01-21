b135f65ee6ff8fad1e0171c6bdb47e6c31b3b5b4
amaliartnaa <amaliaratna41@gmail.com>
2025-01-22 00:24:16 +0700

    feat: add regist validation

--- app/src/main/java/com/example/chatbotavalon/RegisterActivity.kt
+++ app/src/main/java/com/example/chatbotavalon/RegisterActivity.kt
@@ -7,9 +7,12 @@ import android.text.Spanned
 import android.text.method.LinkMovementMethod
 import android.text.style.ClickableSpan
 import android.text.style.ForegroundColorSpan
+import android.util.Patterns
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 
@@ -18,7 +21,11 @@ class RegisterActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.register_activity)
 
+        val fullNameEditText = findViewById<EditText>(R.id.nameEditText)
+        val emailEditText = findViewById<EditText>(R.id.registerEmailEditText)
+        val passwordEditText = findViewById<EditText>(R.id.registerPasswordEditText)
         val loginTextView = findViewById<TextView>(R.id.loginRedirect)
+        val registerButton = findViewById<Button>(R.id.registerButton)
 
         val spannable = SpannableString("Already have an account? Login")
 
@@ -37,11 +44,23 @@ class RegisterActivity : AppCompatActivity(){
         loginTextView.text = spannable
         loginTextView.movementMethod = LinkMovementMethod.getInstance()
 
-        val registerButton = findViewById<Button>(R.id.registerButton)
         registerButton.setOnClickListener {
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-            finish()
+            val fullName = fullNameEditText.text.toString().trim()
+            val email = emailEditText.text.toString().trim()
+            val password = passwordEditText.text.toString().trim()
+
+            if (fullName.isEmpty() || email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Please fill in all fields", Toast.LENGTH_SHORT).show()
+            } else if (!fullName.matches(Regex("^[a-zA-Z\\s]+$"))) {
+                Toast.makeText(this, "Full Name must contain only letters", Toast.LENGTH_SHORT).show()
+            } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                Toast.makeText(this, "Invalid email format", Toast.LENGTH_SHORT).show()
+            } else {
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+
         }
     }
 }
